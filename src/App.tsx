045749c634657332de2ec4af8274751f1dfcd472
Simonas Vaišvila <simas.vaisvila@gmail.com>
2021-11-20 15:20:50 +0200

    clean up default project
    * delete test infrastructure
    * adjust typescript settings
    * change formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
@@ -22,5 +22,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
