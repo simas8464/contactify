b6e20e0e8e5c3386dadbf9e274e0e97877a387c3
Simonas Vaišvila <simas.vaisvila@gmail.com>
2021-11-23 01:06:27 +0200

    make checkbox buttons work

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from 'react';
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import './DropdownMenu.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
 export interface DropdownMenuProps {
   showName: boolean;
@@ -17,9 +19,21 @@ export interface DropdownMenuProps {
   menuIconRef: React.RefObject<HTMLButtonElement>;
 }
 
+function getCheckBoxIcon(enabled: boolean): IconDefinition {
+  return enabled ? faCheckSquare : faSquare;
+}
+
 export default function DropdownMenu({
   showMenu,
   setShowMenu,
+  showName,
+  setShowName,
+  showCity,
+  setshowCity,
+  showEmail,
+  setShowEmail,
+  showPhone,
+  setShowPhone,
   menuIconRef,
 }: DropdownMenuProps): JSX.Element {
   const wrapperRef = useRef<HTMLTableElement>(null);
@@ -63,8 +77,8 @@ export default function DropdownMenu({
       <tbody>
         <tr>
           <td>
-            <button>
-              <FontAwesomeIcon icon={faCheckSquare} />
+            <button onClick={() => setShowName(!showName)}>
+              <FontAwesomeIcon icon={getCheckBoxIcon(showName)} />
             </button>
           </td>
           <td>
@@ -73,8 +87,8 @@ export default function DropdownMenu({
         </tr>
         <tr>
           <td>
-            <button>
-              <FontAwesomeIcon icon={faCheckSquare} />
+            <button onClick={() => setshowCity(!showCity)}>
+              <FontAwesomeIcon icon={getCheckBoxIcon(showCity)} />
             </button>
           </td>
           <td>
@@ -83,8 +97,8 @@ export default function DropdownMenu({
         </tr>
         <tr>
           <td>
-            <button>
-              <FontAwesomeIcon icon={faCheckSquare} />
+            <button onClick={() => setShowEmail(!showEmail)}>
+              <FontAwesomeIcon icon={getCheckBoxIcon(showEmail)} />
             </button>
           </td>
           <td>
@@ -93,8 +107,8 @@ export default function DropdownMenu({
         </tr>
         <tr>
           <td>
-            <button>
-              <FontAwesomeIcon icon={faCheckSquare} />
+            <button onClick={() => setShowPhone(!showPhone)}>
+              <FontAwesomeIcon icon={getCheckBoxIcon(showPhone)} />
             </button>
           </td>
           <td>
