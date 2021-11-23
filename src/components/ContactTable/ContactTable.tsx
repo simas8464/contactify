836e20c75229669ba397d32dc9970548cb1108e4
Simonas Vaišvila <simas.vaisvila@gmail.com>
2021-11-24 01:38:27 +0200

    fix fonts

--- src/components/ContactTable/ContactTable.tsx
+++ src/components/ContactTable/ContactTable.tsx
@@ -73,7 +73,7 @@ export default function ContactTable({
                 <input type='text'></input>
               </div>
               <div>
-                <p>Contactify</p>
+                <p>CONTACTIFY</p>
               </div>
             </div>
           </th>
